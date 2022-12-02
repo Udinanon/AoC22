40f8eca7cbea43e36a0593ca554b7628ad65faff
Udinanon <m.trapanotto@tutamail.com>
2022-12-02 17:26:21 +0100

    Same method as problem 1

--- day2/helper.py
+++ day2/helper.py
@@ -1,19 +1,21 @@
 from itertools import product
-values = ["score += 3+1;",
-          "score += 6+2;",
-          "score += 0+3;",
-          "score += 0+1;",
-          "score += 3+2;",
-          "score += 6+3;",
-          "score += 0+1;",
-          "score += 6+2;",
-          "score += 3+3;"
+# scores R1 P2 S3
+# AR BP CS
+values = ["0+3;",#ax
+          "3+1;",#ay
+          "6+2;",#az
+          "0+1;",#bx
+          "3+2;",#by
+          "6+3;",#bz
+          "0+2;",#cx
+          "3+3;",#cy
+          "6+1;"
           ]
 count = 0
 with open("out.txt", "w") as file_writer:
     for i in product(["A", "B", "C"], ["X", "Y", "Z"]):
         file_writer.writelines(f"""      else if(data[0] == '{i[0]}' && data[2] == '{i[1]}') {{
-            {values[count]}
+            score += {values[count]}
         }}    
 """)
         count+=1
