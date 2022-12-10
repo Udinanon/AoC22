16b073e21977a2e0f567786163685189948e5b2d
Udinanon <m.trapanotto@tutamail.com>
2022-12-10 19:11:23 +0100

    D10 P2

--- day10/problem.c
+++ day10/problem.c
@@ -8,19 +8,32 @@ struct CPU{
     int total;
 };
 
-void noop_CPU(struct CPU* cpu){ // wait for 1 cycle and update total if needed
+
+// the actual code that runs for 1 cycle
+void wait_cycle(struct CPU* cpu){ // wait for 1 cycle and update total if needed
+    // update screen
+    if ((cpu->cycle % 40) == 0)
+    {
+        printf("\n");
+    }
+    int sprite_cent = cpu->reg;
+    int screen_pos = cpu->cycle %40;
+    if (screen_pos == sprite_cent - 1 || screen_pos == sprite_cent || screen_pos == sprite_cent + 1)
+    {
+        printf("#");
+    }
+    else
+    {
+        printf(".");
+    }
+
+    // update CPU
     cpu->cycle++;
     if (((cpu->cycle - 20) % 40) == 0){
         cpu->total+=(cpu->reg*cpu->cycle);
     }
-}
 
-void addx_CPU(struct CPU* cpu, int add){ // wait 2 times and then add value
-    noop_CPU(cpu);
-    noop_CPU(cpu);
-    cpu->reg+=add;
 }
-
 
 int main(void){
     FILE *fp; // open file
@@ -42,17 +55,20 @@ int main(void){
     struct CPU cpu = {0, 1, 0};
 
     while ((line_size >= 0)){
-        if(line_buf[0]=='n'){
-            noop_CPU(&cpu);
-        } else if (line_buf[0]=='a'){
+        if(line_buf[0]=='n'){ // do nothing
+            wait_cycle(&cpu);
+        } else if (line_buf[0]=='a'){ // add value
             int value = strtol(line_buf+5, NULL, 10);
-            addx_CPU(&cpu, value);
+            wait_cycle(&cpu);
+            wait_cycle(&cpu);
+            cpu.reg += value;
         }
+
 
         line_count++;
         line_size = getline(&line_buf, &line_buf_size, fp);
     }
-    printf("TOTAL %d, N_CYCLES %d\n", cpu.total, cpu.cycle);
+    printf("\nTOTAL %d, N_CYCLES %d\n", cpu.total, cpu.cycle);
 
     free(line_buf);
 }
